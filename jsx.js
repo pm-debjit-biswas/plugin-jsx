180c93949f24a1287cb9e4090f6c25132203e8dd
Debjit Biswas <debjit@fastmail.fm>
2015-03-08 22:40:49 +0530

    Add module option while transforming

--- jsx.js
+++ jsx.js
@@ -4,7 +4,9 @@
 //var babel = require('babel-core');
 
 exports.translate = function(load) {
-	var output = babel.transform(load.source);
+	var output = babel.transform(load.source, {
+        modules: 'system'
+    });
 	load.source = output.code;
 	load.metadata.sourceMap = output.sourceMap;
 };
